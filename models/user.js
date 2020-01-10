275f45e33ca0102db1d63ce6601e1f83e2afa935
André <andre_t3lo@hotmail.com>
2020-01-10 20:47:14 +0000

    fix

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoCongig = require('../mongoConfig');
 
 function insertUser(c,t,callback){
     var db = mongoCongig.getDB();
-    var line = {email:c, password:t ,num_games=0, num_victories=0 ,game_id=""};
+    var line = {email:c, password:t ,num_games:0, num_victories:0 ,game_id:""};
     db.collection("users").insertOne(line,function(err, res){
         if(err)
             callback("Error inserting user");
